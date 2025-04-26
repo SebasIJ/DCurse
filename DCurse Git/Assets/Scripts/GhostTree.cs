5a338680ce3732901163c925576bca7307e18db6
SebasIJ <sebastianiguaj@gmail.com>
2025-04-26 16:57:15 -0400

    Tier 3 gameplay
    implemented the ghost enemy that uses tree behavior

--- DCurse Git/Assets/Scripts/GhostTree.cs
+++ DCurse Git/Assets/Scripts/GhostTree.cs
@@ -24,9 +24,13 @@ public class GhostTree : MonoBehaviour
 
     private ghostBehavior root;
     private ghostBehavior currentBehavior;
+    private Vector3 target;
+    private int moveCooldown = 0;
     System.Random RNG = new System.Random();
-    
+
     public PlayerScript player;
+    public Transform sprite;
+    public GameObject fireBall;
 
 
     // Start is called before the first frame update
@@ -36,14 +40,14 @@ public class GhostTree : MonoBehaviour
         root.beID = 0;
 
         ghostBehavior temp = root;
-        if(temp.left == null)
+        if (temp.left == null)
         {
             ghostBehavior left1 = new ghostBehavior();
             left1.beID = 1;
 
             temp.left = left1;
 
-            ghostBehavior temptemp = temp;
+            ghostBehavior temptemp = temp.left;
             if (temptemp.left == null)
             {
                 ghostBehavior left11 = new ghostBehavior();
@@ -52,7 +56,7 @@ public class GhostTree : MonoBehaviour
                 temptemp.left = left11;
             }
 
-            if(temptemp.right == null)
+            if (temptemp.right == null)
             {
                 ghostBehavior right12 = new ghostBehavior();
                 right12.beID = 12;
@@ -68,7 +72,7 @@ public class GhostTree : MonoBehaviour
 
             temp.right = right2;
 
-            ghostBehavior temptemp = temp;
+            ghostBehavior temptemp = temp.right;
             if (temptemp.left == null)
             {
                 ghostBehavior left21 = new ghostBehavior();
@@ -92,11 +96,20 @@ public class GhostTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (player.transform.position.x > transform.position.x)
+        {
+            sprite.transform.localScale = new Vector3(-1, 1, 1); ;
+        }
+        else
+        {
+            sprite.transform.localScale = new Vector3(1, 1, 1);
+        }
+
         switch (currentBehavior.beID)
         {
             case 0:
 
-                if(RNG.Next(1) > 0)
+                if (RNG.Next(2) == 0)
                 {
                     currentBehavior = currentBehavior.left;
                 }
@@ -109,7 +122,7 @@ public class GhostTree : MonoBehaviour
 
             case 1:
 
-                if (RNG.Next(1) > 0)
+                if (RNG.Next(2) == 0)
                 {
                     currentBehavior = currentBehavior.left;
                 }
@@ -122,7 +135,7 @@ public class GhostTree : MonoBehaviour
 
             case 2:
 
-                if (RNG.Next(1) > 0)
+                if (RNG.Next(2) == 0)
                 {
                     currentBehavior = currentBehavior.left;
                 }
@@ -135,26 +148,77 @@ public class GhostTree : MonoBehaviour
 
             case 11:
 
+                GameObject newFire = Instantiate(fireBall, player.transform.position + new Vector3(0,10,0), player.transform.rotation);
+                newFire.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
 
                 currentBehavior = root;
                 break;
 
             case 12:
 
+                if (moveCooldown == 0)
+                {
+                    target = player.transform.position;
+                    moveCooldown = 1;
+                }
 
-                currentBehavior = root;
+                if (transform.position != target)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, target, 5 * Time.deltaTime);
+                }
+                else
+                {
+                    moveCooldown = 0;
+                    currentBehavior = root;
+                }
+
                 break;
 
             case 21:
 
+                if (moveCooldown == 0)
+                {
+                    if (RNG.Next(2) == 0)
+                    {
+                        transform.position = new Vector3(player.transform.position.x - 10, player.transform.position.y, player.transform.position.z);
+                    }
+                    else
+                    {
+                        transform.position = new Vector3(player.transform.position.x + 10, player.transform.position.y, player.transform.position.z);
+                    }
+                    moveCooldown = 120;
+                }
+                else
+                {
+                    moveCooldown--;
 
-                currentBehavior = root;
+                    if (moveCooldown == 0)
+                    {
+                        currentBehavior = root;
+                    }
+
+                }
+
                 break;
 
             case 22:
 
+                if (moveCooldown == 0)
+                {
+                    transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 10, player.transform.position.z);
+                    moveCooldown = 120;
+                }
+                else
+                {
+                    moveCooldown--;
 
-                currentBehavior = root;
+                    if (moveCooldown == 0)
+                    {
+                        currentBehavior = root;
+                    }
+
+                }
+
                 break;
         }
     }
